e1d11a71fb0d618cb614004b5c621577664ae05a
boruch d <dubywider@gmail.com>
2019-05-09 16:28:50 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,10 +3,11 @@ def reformat_languages(languages)
   new_hash = {} 
 
   languages.map do |style, description|
-    binding.pry
+   
     description.map do |lang, type|
       if new_hash.has_key?(lang)
         new_hash[lang][:style] << style
+        
       else
         new_hash[lang] = type
         new_hash[lang][:style] = [style]
