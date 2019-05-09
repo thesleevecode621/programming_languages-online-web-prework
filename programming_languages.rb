6fa1fbdcf0673dfd63bee02ff934dfc1862467fc
boruch d <dubywider@gmail.com>
2019-05-09 16:25:12 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,9 @@
+
 def reformat_languages(languages)
   new_hash = {} 
 
   languages.map do |style, description|
-    require 'pry'
+    
     description.map do | type|
       if new_hash.has_key?(lang)
         new_hash[lang][:style] << style
